3398ab09f05df6340b1a6c6155e24a5dcf00b621
ralvarez14 <ralvarezmoreno99@gmail.com>
2017-02-12 13:02:03 -0700

    Update MainActivity.java

--- app/src/main/java/edu/utep/cs/cs4330/battleship/MainActivity.java
+++ app/src/main/java/edu/utep/cs/cs4330/battleship/MainActivity.java
@@ -39,6 +39,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTouch(int x, int y) {
                 toast(String.format("Touched: %d, %d", x, y));
+                
+                countNumOfHits++;
+                        counter.setText(String.valueOf("Number of Shots:" + countNumOfHits));
+                        toast(String.format("Touched: %d, %d", x, y));
+                        for(int i = 0; i < player.battleship.getBattleshipCoordinates().size(); i++){
+                            // (LEFTMOST, RIGHTMOST), (LEFTMOST+1, RIGHTMOST+1)... (LEFTMOST+N, RIGHTMOST+N)
+                            String leftMost = String.valueOf(player.battleship.getBattleshipCoordinates().get(i).charAt(0));
+                            String rightMost = String.valueOf(player.battleship.getBattleshipCoordinates().get(i).charAt(2));
+                            if((String.valueOf(x).equals(leftMost)) && (String.valueOf(y).equals(rightMost))) {
+                                Log.w("Critical hit! X:", String.valueOf(x));
+                                Log.w("Critical hit! Y:", String.valueOf(y));
+                                makeExplosionSound();
+                            }
+                            else{
+                                Log.w("Missed!", "");
+                            }
+                        }
+                    }
 
             }
         });
@@ -55,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
                         countNumOfHits++;
                         counter.setText(String.valueOf("Number of Shots:" + countNumOfHits));
                         toast(String.format("Touched: %d, %d", x, y));
+                        <!--
                         for(int i = 0; i < player.battleship.getBattleshipCoordinates().size(); i++){
                             // (LEFTMOST, RIGHTMOST), (LEFTMOST+1, RIGHTMOST+1)... (LEFTMOST+N, RIGHTMOST+N)
                             String leftMost = String.valueOf(player.battleship.getBattleshipCoordinates().get(i).charAt(0));
@@ -67,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
                             else{
                                 Log.w("Missed!", "");
                             }
-                        }
+                        } -->
                     }
                 });
             }
