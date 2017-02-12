f0cb28124973a2612bd83e05622f0afefc293d9e
Oscar Ricaud <oiricaud@miners.utep.edu>
2017-02-11 18:36:18 -0700

    Battleship coordinates is now working. If the user guesses correctly where the ship is it makes a cool sound effect. Now I must do the rest for the boats while checking the coordinates do not intersect.

--- app/src/main/java/edu/utep/cs/cs4330/battleship/MainActivity.java
+++ app/src/main/java/edu/utep/cs/cs4330/battleship/MainActivity.java
@@ -2,9 +2,11 @@ package edu.utep.cs.cs4330.battleship;
 
 
 import android.content.Context;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -35,41 +37,47 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTouch(int x, int y) {
                 toast(String.format("Touched: %d, %d", x, y));
+
             }
         });
         Button newButton = (Button) findViewById(R.id.newButton);
         newButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                // showPopUp(); Notify the user if he/she wants to create a new game
-                // After that
-                //drawPlaces();
-                //boardView.drawPlaces(Canvas canvas);
+                final Player player = new Player();
+                player.setUpBoats();
+                boardView.addBoardTouchListener(new BoardView.BoardTouchListener() {
+                    @Override
+                    public void onTouch(int x, int y) {
+                        toast(String.format("Touched: %d, %d", x, y));
+                        for(int i = 0; i < player.battleship.getWhereisBattleship().size(); i++){
+                            // (LEFTMOST, RIGHTMOST), (LEFTMOST+1, RIGHTMOST+1)... (LEFTMOST+N, RIGHTMOST+N)
+                            String leftMost = String.valueOf(player.battleship.getWhereisBattleship().get(i).charAt(0));
+                            String rightMost = String.valueOf(player.battleship.getWhereisBattleship().get(i).charAt(2));
+                            if((String.valueOf(x).equals(leftMost)) && (String.valueOf(y).equals(rightMost))) {
+                                Log.w("Critical hit! X:", String.valueOf(x));
+                                Log.w("Critical hit! Y:", String.valueOf(y));
+                                makeExplosionSound();
+                            }
+                            else{
+                                Log.w("Missed!", "");
+                            }
+                        }
+                    }
+                });
             }
         });
     }
 
-    /*
-    private void showPopUp() {
-        try {
-// We need to get the instance of the LayoutInflater
-            LayoutInflater inflater = (LayoutInflater) MainActivity.this
-            getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View layout = inflater.inflate(R.layout.popup,
-                    (ViewGroup) findViewById(R.id.popup_1));
-            pw = new PopupWindow(layout, 300, 370, true);
-            pw.showAtLocation(layout, Gravity.CENTER, 0, 0);
-            Close = (Button) layout.findViewById(R.id.close_popup);
-            Close.setOnClickListener(cancel_button);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    private void makeExplosionSound() {
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.torpedo);
+        Log.v("noiseee", "Playing sound...");
+        mp.start();
     }
-    }
-    */
 
     /** Show a toast message. */
     protected void toast(String msg) {
-        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
