fbcd5b759ad0edfe7eb0b60c71bf2a285fb5dcf4
Oscar Ricaud <oiricaud@miners.utep.edu>
2017-02-11 18:48:45 -0700

    Refactoring...

--- app/src/main/java/edu/utep/cs/cs4330/battleship/Battleship.java
+++ app/src/main/java/edu/utep/cs/cs4330/battleship/Battleship.java
@@ -4,21 +4,22 @@ import android.util.Log;
 
 import java.util.ArrayList;
 
-public class Battleship{
+public class Battleship extends FleetShip {
     private int battleshipSize = 4;
-    private boolean hasCoordinates = false; // 0 for false, 1 for true
+    private boolean hasCoordinates = false;
     private int[] coordinates = new int[2];
     private int position = 0; // Default position is set to horizontal, 1 = Vertical
     private int [] battleship = new int [battleshipSize];
-    ArrayList<String> battleshipFinalCoordinates = new ArrayList<String>();
+    private ArrayList<String> battleshipFinalCoordinates = new ArrayList<String>();
+    private ArrayList<String> whereisBattleship;
 
     public void placeShip(int boardsize) {
-        int[] coorRandom = getRandomCoordinate(boardsize);
+        int[] coorRandom = getRandomCoordinates(boardsize);
         coordinates[0] = coorRandom[0];
         coordinates[1] = coorRandom[1];
     }
 
-    private int[] getRandomCoordinate(int boardsize) {
+    private int[] getRandomCoordinates(int boardsize) {
         int [] coordinates = new int[2];
                 if(!hasCoordinates) {
                     int randPosition = 2;
@@ -36,18 +37,30 @@ public class Battleship{
                             Log.w(" Position is horizontal", String.valueOf(position));
                             battleship[i] = coordinates[0];
                             battleshipFinalCoordinates.add(i, coordinates[0]+ " "+ coordinates[1]);
+                            boatsCoordinates[coordinates[0]][battleship[i]] = 1;
                             coordinates[1]++;
                         }
                         if(position == 1) { // Position of boat is vertical
                             Log.w(" Position is vertical", String.valueOf(position));
                             battleship[i] = coordinates[1];
                             battleshipFinalCoordinates.add(i, coordinates[0]+ " "+ battleship[i]);
+                            boatsCoordinates[coordinates[0]][battleship[i]] = 1;
                             coordinates[0]++;
                         }
                     }
                     Log.w("Battleship Position:", String.valueOf(battleshipFinalCoordinates));
+                    setBattleshipPosition(battleshipFinalCoordinates);
                     hasCoordinates = true;
                 }
         return coordinates;
+    }
+
+    public ArrayList<String> getBattleshipCoordinates() {
+        return whereisBattleship;
+    }
+
+    public void setBattleshipPosition(ArrayList<String> whereisBattleship) {
+        getboatsCoordinates();
+        this.whereisBattleship = whereisBattleship;
     }
 }
