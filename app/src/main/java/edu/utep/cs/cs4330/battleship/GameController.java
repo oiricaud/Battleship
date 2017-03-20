e841ad6d1476317113f082f54e99d6c7d1400a69
Oscar Ricaud <oiricaud@miners.utep.edu>
2017-03-19 20:49:10 -0600

    Separated all views from the controller class! Woo

--- app/src/main/java/edu/utep/cs/cs4330/battleship/GameController.java
+++ app/src/main/java/edu/utep/cs/cs4330/battleship/GameController.java
@@ -15,9 +15,7 @@ import android.widget.TextView;
  * Last update: 02/23/2017
  */
 public class GameController extends AppCompatActivity {
-    //private TextView counter;
     private Font eightBitFont = new Font("fonts/eightbit.TTF");
-
     private String difficulty;
     private boolean isComputerReady;
     private boolean isHumanReady;
@@ -27,31 +25,30 @@ public class GameController extends AppCompatActivity {
          * @return this returns the difficulty the user chose, which is later retrieved to be displayed
          * on the android phone.
          */
-    public String getDifficulty() {
-        return difficulty;
-    }
+        public String getDifficulty() {
+            return difficulty;
+        }
 
-    public void setDifficulty(String difficulty) {
-        this.difficulty = difficulty;
-    }
+        public void setDifficulty(String difficulty) {
+            this.difficulty = difficulty;
+        }
 
-    public boolean isComputerReady() {
-        return isComputerReady;
-    }
+        public boolean isComputerReady() {
+            return isComputerReady;
+        }
 
-    public void setComputerReady(boolean computerReady) {
-        isComputerReady = computerReady;
-    }
+        public void setComputerReady(boolean computerReady) {
+            isComputerReady = computerReady;
+        }
 
-    public boolean isHumanReady() {
-        return isHumanReady;
-    }
+        public boolean isHumanReady() {
+            return isHumanReady;
+        }
 
-    public void setHumanReady(boolean humanReady) {
-        isHumanReady = humanReady;
-    }
+        public void setHumanReady(boolean humanReady) {
+            isHumanReady = humanReady;
+        }
     /* End Setters and Getters */
-
 
     /** This is the main controller class and handles the creation of multiple ships and board.
      //    *  @see Ship.java
@@ -67,22 +64,22 @@ public class GameController extends AppCompatActivity {
      *  and colors a red circle the position of the boats, else colors a white circle indicating the
      *  user missed.
      *
-     *
      * @param savedInstanceState is the starting state.
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Calls the corresponding controllers
         if(savedInstanceState == null) {
             Bundle extras = getIntent().getExtras();
             if (extras == null) {
-                launchHomeController(); // Launch the first activity, the starting state, s0.
+                launchHomeController(); // By default this controller is called
             } else{
                 String controller = extras.getString("controllerName");
+
                 if (controller.equals("computerPlaceBoatsController")) {
-                    computerPlaceBoatsController(); // This means that the human has already placed the boats
-                                            // and chosen a difficulty level
+                    computerPlaceBoatsController();
                 }
                 if (controller.equals("startGameController")) {
                     startGameController();
@@ -92,7 +89,6 @@ public class GameController extends AppCompatActivity {
                 }
             }
         }
-
     }
     /**
      * This is state 0, I will represent states such as s0, s1, s2, ... sn. Hopefully this makes it
@@ -111,7 +107,6 @@ public class GameController extends AppCompatActivity {
         Intent intent = new Intent(GameController.this, GameView.class);
         String level_of_difficulty = String.valueOf(getDifficulty());
         intent.putExtra("level_of_difficulty", level_of_difficulty); // YOUR key, variable you are passing
-        intent.putExtra("userType", "null");
         intent.putExtra("shouldWeStartGame", "false");
         intent.putExtra("viewWeWantToLaunch", "launchHomeView");
         GameController.this.startActivity(intent);
@@ -121,56 +116,17 @@ public class GameController extends AppCompatActivity {
      *  easy, medium or hard, @see layout/activity_level for more details.
      */
     private void humanChooseLevelController() {
-        setContentView(R.layout.activity_level);
-        Button easy = (Button) findViewById(R.id.easy);
-        Button medium = (Button) findViewById(R.id.medium);
-        Button hard = (Button) findViewById(R.id.hard);
-        TextView chooseLevel = (TextView) findViewById(R.id.chooseDifficulty);
-
-        // Change font to a cooler 8-bit font.
-        eightBitFont.changeFont(this, easy);
-        eightBitFont.changeFont(this, medium);
-        eightBitFont.changeFont(this, hard);
-        eightBitFont.changeFont(this, chooseLevel);
-
-        // Wait for the user input
-        easy.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                setDifficulty("easy");
-                callHumanPlaceBoatsView();  // Takes the user to @see GameView to place ships on the grid.
-            }
-        });
-        medium.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                setDifficulty("medium");
-                callHumanPlaceBoatsView(); // Takes the user to @see GameView to place ships on the grid.
-            }
-        });
-        hard.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                setDifficulty("hard");
-                callHumanPlaceBoatsView(); // Takes the user to @see GameView to place ships on the grid.
-            }
-        });
-    }
-    /** Where the magic happens. Aka the function that allows the human to place boats on board view.
-     *  s0->s1->s2
-     *  @see GameView for more details.
-     */
-    private void callHumanPlaceBoatsView() {
         // The following is how you send data to other classes.
-        setHumanReady(true);
         Intent intent = new Intent(GameController.this, GameView.class);
         String level_of_difficulty = String.valueOf(getDifficulty());
         intent.putExtra("level_of_difficulty", level_of_difficulty); // YOUR key, variable you are passing
-        intent.putExtra("viewWeWantToLaunch", "humanPlaceBoatsView");
         intent.putExtra("shouldWeStartGame", "false");
+        intent.putExtra("viewWeWantToLaunch", "humanChooseLevelView");
         GameController.this.startActivity(intent);
         GameController.this.overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        setHumanReady(true);
     }
+
     /** s0->s1->s2->s3
      *
      * Changes the content view to a more grey, unnerving background. Also changes the song to more
