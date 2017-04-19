c940f6e2258622f3c4829cbc63320921e968ec69
ralvarez14 <ralvarezmoreno99@gmail.com>
2017-04-18 20:54:09 -0600

    add files via upload
    Adding constants & bluetooth controller

---
+++ Constants.java
@@ -0,0 +1,20 @@
+package edu.utep.cs.cs4330.battleship;
+
+/**
+ * Created by ralva2 on 4/18/17.
+ */
+
+public interface Constants {
+
+    // Message types sent from the BluetoothChatService Handler
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+
+    // Key names received from the BluetoothChatService Handler
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+
+}
